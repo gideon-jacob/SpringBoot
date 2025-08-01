6040d211269083e39b6c36cd1717b8accfc8cd7d
Gideon Jacob <gideonjacob.in@gmail.com>
2025-08-01 14:21:01 +0530

    update

--- src/main/java/org/prathyusha/menteemanagementsystem/MenteeManagementSystemController.java
+++ src/main/java/org/prathyusha/menteemanagementsystem/MenteeManagementSystemController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class MenteeMgmtSystemController {
     
-    @GetMapping('/')
+    @GetMapping("/")
     public String sayHello() {
         return "Hello there!!";
     }
